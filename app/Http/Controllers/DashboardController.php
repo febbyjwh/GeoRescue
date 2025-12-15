bc9bc5ee2e0ed91a2517dc90de78e3388e890d56
radindaptri <radindasalsabila25@gmail.com>
2025-12-15 23:07:58 +0700

    perubahan lokal sebelum pull

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function dashboard(){
-        return view ('Admin.Dashboard');
+    public function index()
+    {
+        return view('dashboard');
     }
 }
