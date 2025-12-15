938833035a837178a745ec684a5b44d1800c6398
Febby Juwita Hizriani <febbyjuitah@gmail.com>
2025-12-15 22:15:52 +0700

    templating

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -42,8 +42,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password'
-             => 'hashed',
+            'password' => 'hashed',
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,39 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
-
     public function run(): void
     {
         // User::factory(10)->create();
-        // Role 
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'user']);
 
-        // Buat user admin
-        $admin = User::factory()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
-        ]);
-        $admin->assignRole('admin');
-
-        // Buat user biasa
-        $user = User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
-            'email' => 'user@example.com',
-            'password' => bcrypt('password'),
+            'email' => 'test@example.com',
         ]);
-        $user->assignRole('user');
     }
 }
